37670b52d94ae2c8fca12f6d094fdbe0fb5d4a60
kusumaakter <kusumaakter2003.kulkandi@gmail.com>
2023-08-27 15:57:28 +0600

    Algo

---
+++ Another User/Algorithm/3.bfs_new.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>vec[1000];
+bool visited[1000];
+
+void bfs(int s)
+{
+     queue<int>q;
+     q.push(s);
+     visited[s]=true;
+
+     while(!q.empty())
+     {
+        int u=q.front();
+        q.pop();
+        cout<<u<<" ";
+
+        for(int i=0;i<vec[u].size();i++)
+        {
+            int v=vec[u][i];
+            if(!visited[v])
+            {
+                q.push(v);
+            }
+            visited[v]=true;
+        }
+     }
+}
+
+int main()
+{
+     
+     int n,m;
+     cin>>n>>m;
+     for(int i=0;i<m;i++)
+     {
+        int u,v;
+        cin>>u>>v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+
+     }
+     bfs(1);
+}

ebfb0d52095761a783e7ec2cb9649268e9488c64
kusumaakter <kusumaakter2003.kulkandi@gmail.com>
2023-08-29 13:37:04 +0600

    Algo

--- Another User/Algorithm/week-02/module-08/1.dis.cpp
+++ Another User/Algorithm/week-02/module-08/1.dis.cpp
@@ -7,23 +7,29 @@ const int MAX_N = 1005;
 vector<int> graph[MAX_N];
 bool visited[MAX_N];
 
-void addEdge(int u, int v) {
+void addEdge(int u, int v)
+{
     graph[u].push_back(v);
     graph[v].push_back(u);
 }
 
-int dfs(int node, int destination, int distance) {
-    if (node == destination) {
+int dfs(int node, int destination, int distance)
+{
+    if (node == destination)
+    {
         return distance;
     }
 
     visited[node] = true;
     int minDistance = -1;
 
-    for (int neighbor : graph[node]) {
-        if (!visited[neighbor]) {
+    for (int neighbor : graph[node])
+    {
+        if (!visited[neighbor])
+        {
             int tempDistance = dfs(neighbor, destination, distance + 1);
-            if (tempDistance != -1 && (minDistance == -1 || tempDistance < minDistance)) {
+            if (tempDistance != -1 && (minDistance == -1 || tempDistance < minDistance))
+            {
                 minDistance = tempDistance;
             }
         }
@@ -33,11 +39,13 @@ int dfs(int node, int destination, int distance) {
     return minDistance;
 }
 
-int main() {
+int main()
+{
     int N, E;
     cin >> N >> E;
 
-    for (int i = 0; i < E; ++i) {
+    for (int i = 0; i < E; ++i)
+    {
         int u, v;
         cin >> u >> v;
         addEdge(u, v);
@@ -46,10 +54,11 @@ int main() {
     int Q;
     cin >> Q;
 
-    for (int i = 0; i < Q; ++i) {
+    for (int i = 0; i < Q; ++i)
+    {
         int source, destination;
         cin >> source >> destination;
-        
+
         memset(visited, false, sizeof(visited));
         int minDistance = dfs(source, destination, 0);
         cout << minDistance << endl;
